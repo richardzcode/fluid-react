ea9caaa639849095caa5f0578d3c76c55b06a2e2
Richard Zhang <richardzcode@gmail.com>
2018-01-03 19:42:29 -0800

    more dom elements

--- dist/components/Dom.js
+++ dist/components/Dom.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.A = exports.Pre = exports.Button = exports.Textarea = exports.Input = exports.Li = exports.Ul = exports.Span = exports.Div = undefined;
+exports.Hr = exports.H6 = exports.H5 = exports.H4 = exports.H3 = exports.H2 = exports.H1 = exports.A = exports.Code = exports.Pre = exports.Button = exports.Option = exports.Select = exports.Textarea = exports.Input = exports.Form = exports.Footer = exports.Header = exports.Section = exports.Li = exports.Ul = exports.Label = exports.Span = exports.P = exports.Div = undefined;
 
 var _react = require('react');
 
@@ -24,6 +24,15 @@ var _Div = function _Div(props) {
 };
 var Div = (0, _css.withMediaQuery)(_Div);
 
+var _P = function _P(props) {
+    return _react2.default.createElement(
+        'p',
+        props,
+        props.children
+    );
+};
+var P = (0, _css.withMediaQuery)(_P);
+
 var _Span = function _Span(props) {
     return _react2.default.createElement(
         'span',
@@ -32,6 +41,15 @@ var _Span = function _Span(props) {
     );
 };
 var Span = (0, _css.withMediaQuery)(_Span);
+
+var _Label = function _Label(props) {
+    return _react2.default.createElement(
+        'label',
+        props,
+        props.children
+    );
+};
+var Label = (0, _css.withMediaQuery)(_Label);
 
 var _Ul = function _Ul(props) {
     return _react2.default.createElement(
@@ -51,6 +69,42 @@ var _Li = function _Li(props) {
 };
 var Li = (0, _css.withMediaQuery)(_Li);
 
+var _Section = function _Section(props) {
+    return _react2.default.createElement(
+        'section',
+        props,
+        props.children
+    );
+};
+var Section = (0, _css.withMediaQuery)(_Section);
+
+var _Header = function _Header(props) {
+    return _react2.default.createElement(
+        'header',
+        props,
+        props.children
+    );
+};
+var Header = (0, _css.withMediaQuery)(_Header);
+
+var _Footer = function _Footer(props) {
+    return _react2.default.createElement(
+        'footer',
+        props,
+        props.children
+    );
+};
+var Footer = (0, _css.withMediaQuery)(_Footer);
+
+var _Form = function _Form(props) {
+    return _react2.default.createElement(
+        'form',
+        props,
+        props.children
+    );
+};
+var Form = (0, _css.withMediaQuery)(_Form);
+
 var _Input = function _Input(props) {
     return _react2.default.createElement(
         'input',
@@ -68,6 +122,24 @@ var _Textarea = function _Textarea(props) {
     );
 };
 var Textarea = (0, _css.withMediaQuery)(_Textarea);
+
+var _Select = function _Select(props) {
+    return _react2.default.createElement(
+        'select',
+        props,
+        props.children
+    );
+};
+var Select = (0, _css.withMediaQuery)(_Select);
+
+var _Option = function _Option(props) {
+    return _react2.default.createElement(
+        'option',
+        props,
+        props.children
+    );
+};
+var Option = (0, _css.withMediaQuery)(_Option);
 
 var _Button = function _Button(props) {
     return _react2.default.createElement(
@@ -87,6 +159,15 @@ var _Pre = function _Pre(props) {
 };
 var Pre = (0, _css.withMediaQuery)(_Pre);
 
+var _Code = function _Code(props) {
+    return _react2.default.createElement(
+        'code',
+        props,
+        props.children
+    );
+};
+var Code = (0, _css.withMediaQuery)(_Code);
+
 var _A = function _A(props) {
     return _react2.default.createElement(
         'a',
@@ -96,12 +177,91 @@ var _A = function _A(props) {
 };
 var A = (0, _css.withMediaQuery)(_A);
 
+var _H1 = function _H1(props) {
+    return _react2.default.createElement(
+        'h1',
+        props,
+        props.children
+    );
+};
+var H1 = (0, _css.withMediaQuery)(_H1);
+
+var _H2 = function _H2(props) {
+    return _react2.default.createElement(
+        'h2',
+        props,
+        props.children
+    );
+};
+var H2 = (0, _css.withMediaQuery)(_H2);
+
+var _H3 = function _H3(props) {
+    return _react2.default.createElement(
+        'h3',
+        props,
+        props.children
+    );
+};
+var H3 = (0, _css.withMediaQuery)(_H3);
+
+var _H4 = function _H4(props) {
+    return _react2.default.createElement(
+        'h4',
+        props,
+        props.children
+    );
+};
+var H4 = (0, _css.withMediaQuery)(_H4);
+
+var _H5 = function _H5(props) {
+    return _react2.default.createElement(
+        'h5',
+        props,
+        props.children
+    );
+};
+var H5 = (0, _css.withMediaQuery)(_H5);
+
+var _H6 = function _H6(props) {
+    return _react2.default.createElement(
+        'h6',
+        props,
+        props.children
+    );
+};
+var H6 = (0, _css.withMediaQuery)(_H6);
+
+var _Hr = function _Hr(props) {
+    return _react2.default.createElement(
+        'hr',
+        props,
+        props.children
+    );
+};
+var Hr = (0, _css.withMediaQuery)(_Hr);
+
 exports.Div = Div;
+exports.P = P;
 exports.Span = Span;
+exports.Label = Label;
 exports.Ul = Ul;
 exports.Li = Li;
+exports.Section = Section;
+exports.Header = Header;
+exports.Footer = Footer;
+exports.Form = Form;
 exports.Input = Input;
 exports.Textarea = Textarea;
+exports.Select = Select;
+exports.Option = Option;
 exports.Button = Button;
 exports.Pre = Pre;
+exports.Code = Code;
 exports.A = A;
+exports.H1 = H1;
+exports.H2 = H2;
+exports.H3 = H3;
+exports.H4 = H4;
+exports.H5 = H5;
+exports.H6 = H6;
+exports.Hr = Hr;
--- src/components/Dom.js
+++ src/components/Dom.js
@@ -8,10 +8,20 @@ const _Div = (props) => (
 )
 const Div = withMediaQuery(_Div);
 
+const _P = (props) => (
+    <p {...props}>{props.children}</p>
+)
+const P = withMediaQuery(_P);
+
 const _Span = (props) => (
     <span {...props}>{props.children}</span>
 )
 const Span = withMediaQuery(_Span);
+
+const _Label = (props) => (
+    <label {...props}>{props.children}</label>
+)
+const Label = withMediaQuery(_Label);
 
 const _Ul = (props) => (
     <ul {...props}>{props.children}</ul>
@@ -23,6 +33,26 @@ const _Li = (props) => (
 )
 const Li = withMediaQuery(_Li);
 
+const _Section = (props) => (
+    <section {...props}>{props.children}</section>
+)
+const Section = withMediaQuery(_Section);
+
+const _Header = (props) => (
+    <header {...props}>{props.children}</header>
+)
+const Header = withMediaQuery(_Header);
+
+const _Footer = (props) => (
+    <footer {...props}>{props.children}</footer>
+)
+const Footer = withMediaQuery(_Footer);
+
+const _Form = (props) => (
+    <form {...props}>{props.children}</form>
+)
+const Form = withMediaQuery(_Form);
+
 const _Input = (props) => (
     <input {...props}>{props.children}</input>
 )
@@ -32,6 +62,16 @@ const _Textarea = (props) => (
     <textarea {...props}>{props.children}</textarea>
 )
 const Textarea = withMediaQuery(_Textarea);
+
+const _Select = (props) => (
+    <select {...props}>{props.children}</select>
+)
+const Select = withMediaQuery(_Select);
+
+const _Option = (props) => (
+    <option {...props}>{props.children}</option>
+)
+const Option = withMediaQuery(_Option);
 
 const _Button = (props) => (
     <button {...props}>{props.children}</button>
@@ -43,19 +83,75 @@ const _Pre = (props) => (
 )
 const Pre = withMediaQuery(_Pre);
 
+const _Code = (props) => (
+    <code {...props}>{props.children}</code>
+)
+const Code = withMediaQuery(_Code);
+
 const _A = (props) => (
     <a {...props}>{props.children}</a>
 )
 const A = withMediaQuery(_A);
 
+const _H1 = (props) => (
+    <h1 {...props}>{props.children}</h1>
+)
+const H1 = withMediaQuery(_H1);
+
+const _H2 = (props) => (
+    <h2 {...props}>{props.children}</h2>
+)
+const H2 = withMediaQuery(_H2);
+
+const _H3 = (props) => (
+    <h3 {...props}>{props.children}</h3>
+)
+const H3 = withMediaQuery(_H3);
+
+const _H4 = (props) => (
+    <h4 {...props}>{props.children}</h4>
+)
+const H4 = withMediaQuery(_H4);
+
+const _H5 = (props) => (
+    <h5 {...props}>{props.children}</h5>
+)
+const H5 = withMediaQuery(_H5);
+
+const _H6 = (props) => (
+    <h6 {...props}>{props.children}</h6>
+)
+const H6 = withMediaQuery(_H6);
+
+const _Hr = (props) => (
+    <hr {...props}>{props.children}</hr>
+)
+const Hr = withMediaQuery(_Hr);
+
 export {
     Div,
+    P,
     Span,
+    Label,
     Ul,
     Li,
+    Section,
+    Header,
+    Footer,
+    Form,
     Input,
     Textarea,
+    Select,
+    Option,
     Button,
     Pre,
-    A
+    Code,
+    A,
+    H1,
+    H2,
+    H3,
+    H4,
+    H5,
+    H6,
+    Hr
 };
